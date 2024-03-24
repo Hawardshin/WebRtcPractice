7f9a3745ad888df60bba74548d6160ec914615c0
hawardShin <joushin@student.42seoul.kr>
2024-03-24 11:11:34 +0900

    Feat: WebSocket Server With Stomp lib

---
+++ backend/src/main/java/WebSocketServer/WebSocketServer/Greeting.java
@@ -0,0 +1,15 @@
+package WebSocketServer.WebSocketServer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+
+	private String content;
+}
---
+++ backend/src/main/java/WebSocketServer/WebSocketServer/HelloMessage.java
@@ -0,0 +1,16 @@
+package WebSocketServer.WebSocketServer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloMessage {
+
+	private String name;
+
+}
